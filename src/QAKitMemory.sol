7b89db72583652b476c368322c99f479064092e0
David Meister <thedavidmeister@gmail.com>
2023-03-16 10:16:04 +0000

    workflows

--- src/QAKitMemory.sol
+++ src/QAKitMemory.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.15;
 
 import "sol.lib.bytes/LibBytes.sol";
 
-library QAKitMemory {
+contract QAKitMemory is Test {
     using LibBytes for bytes;
 
     function assertMemoryAlignment() internal {
@@ -15,9 +15,13 @@ library QAKitMemory {
         assertEq(memPtr_ % 0x20, 0);
     }
 
-    /// Solidity manages memory in the following way. There is a “free memory pointer” at position 0x40 in memory.
-    /// If you want to allocate memory, use the memory starting from where this pointer points at and update it.
-    /// **There is no guarantee that the memory has not been used before and thus you cannot assume that its contents are zero bytes.**
+    /// https://docs.soliditylang.org/en/v0.8.17/assembly.html#memory-management
+    /// > Solidity manages memory in the following way. There is a
+    /// > “free memory pointer” at position 0x40 in memory. If you want to
+    /// > allocate memory, use the memory starting from where this pointer points
+    /// > at and update it.
+    /// > **There is no guarantee that the memory has not been used before and
+    /// > thus you cannot assume that its contents are zero bytes.**
     function copyPastAllocatedMemory(bytes memory input_) internal pure {
         Cursor outputCursor_;
         assembly {
