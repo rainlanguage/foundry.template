d29878ff1329892862825a2aed003e94d620fb9d
David Meister <thedavidmeister@gmail.com>
2023-03-23 12:18:21 +0400

    Delete cp-workflows.sh

--- cp-workflows.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-mkdir -p ./.github/workflows
-for f in docs slither test
-do
-  cp -f "./lib/qakit.foundry/.github/workflows/qakit-$f.yaml" "./.github/workflows/qakit-$f.yaml"
-done
